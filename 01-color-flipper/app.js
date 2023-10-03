6e7f8e66ad6c50bacc58a5e9c00cc62c38c519ed
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-10-03 10:30:09 +0200

    code: create button and color variables to manipulate the DOM

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// button.addEventListener("click", function () {
+//   console.log(document.body);
+// });
