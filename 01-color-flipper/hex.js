4feacbd7929ec2abf1325b7130afeaada44c34e0
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-10-03 11:30:33 +0200

    fix: remove bug to enable onClick functionality
    Remove the 'on' word from 'onclick' so that the eventListener could work

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -3,7 +3,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const button = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-button.addEventListener("onclick", function () {
+button.addEventListener("click", function () {
   let hexColor = "#";
 
   for (let i = 0; i < 6; i++) {
