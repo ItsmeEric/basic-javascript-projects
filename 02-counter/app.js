9785a9bd67671961ebd7bfaf8fde0cd857de9ac9
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-10-06 12:35:42 +0200

    feat(click): add eventListener to the buttons

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -4,3 +4,9 @@ let count = 0;
 // Select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+
+// Looping through the buttons
+btns.forEach(function (button) {
+  // Add EventListener and check which  button is being selected
+  button.addEventListener("click", function (e) {});
+});
