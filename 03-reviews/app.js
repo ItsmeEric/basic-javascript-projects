98fd35e6a8752cecee6da4a964caa5f45ddd26a0
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-01 22:27:44 +0200

    [Feature(function)]: add function to handle reviewed person

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -45,14 +45,17 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-});
-// show person based on item
-
+}
 // show next person
 
 // show prev person
