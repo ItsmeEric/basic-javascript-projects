2c5c41e7a55bd5cfb0558006102c01d8d8560fec
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-01 22:35:04 +0200

    [Fix]: fix currentItem coditional bug

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -59,12 +59,18 @@ function showPerson(person) {
 // show next person
 nextBtn.addEventListener("click", function () {
   currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
   showPerson(currentItem);
 });
 
 // show prev person
 prevBtn.addEventListener("click", function () {
   currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
   showPerson(currentItem);
 });
 
