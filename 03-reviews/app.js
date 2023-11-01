51e7841a64b0f5f92fa3a1f894b215e4ef39fb0c
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-01 22:23:22 +0200

    [Feature]: add first review (default review)

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -36,13 +36,21 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-// set starting item
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
-// load initial item
+// set starting item
+let currentItem = 0;
 
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
 // show person based on item
 
 // show next person
