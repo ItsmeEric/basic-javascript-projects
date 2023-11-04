e34698d52da9eb33d6183bf0f39b3c117a40e9fb
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-05 00:17:50 +0200

    [Feature]: add feature to close the sidebar
    When a user opens the sidebar then clicks the 'X' button, the sidebar gets closed

--- 05-sidebar/app.js
+++ 05-sidebar/app.js
@@ -12,3 +12,7 @@ toggleBtn.addEventListener("click", function () {
   // The short way
   sidebar.classList.toggle("show-sidebar");
 });
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
