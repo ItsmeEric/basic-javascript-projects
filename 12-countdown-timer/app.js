0d77460d014b18b0adddf646f64a7fa952f7b173
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-19 00:25:05 +0200

    [Feature]: add dynamic future giveaway date

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -25,3 +25,17 @@ const weekdays = [
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
+
+// Add a specific date
+let futureDate = new Date(2023, 10, 21, 10, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+const day = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year}, ${hours}:${minutes}pm`;
