9cfc1018177c1db44bde61a1e0b6ac0a4b6cbb91
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-20 10:40:38 +0200

    [Feature]: add a dynamic 10 days reset date
    Added a 10 day reset time to be displayed every time the user refresh his screen

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,9 +26,14 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-// Add a specific date
-let futureDate = new Date(2023, 10, 21, 10, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+// Add a specific date
+// let futureDate = new Date(2023, 10, 21, 10, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
