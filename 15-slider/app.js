80ffe626517099be772dc5d8f2c77bed4102ed32
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-11-28 11:04:43 +0200

    [Feature]: create a function to work as a carousel to the images

--- 15-slider/app.js
+++ 15-slider/app.js
@@ -5,3 +5,21 @@ const prevBtn = document.querySelector(".prevBtn");
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
+
+let counter = 0;
+
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
