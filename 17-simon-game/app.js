b84f0eba4d4b21a0737aff2ae0a2ab915491576f
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-06 22:28:11 +0200

    🎉[Feature]: add event listeners to the button, to give it initial logic

--- 17-simon-game/app.js
+++ 17-simon-game/app.js
@@ -4,7 +4,7 @@ let flash;
 let turn;
 let good;
 let computerTurn;
-let interval;
+let intervalId;
 let strict = false;
 let noise = true;
 let on = false;
@@ -18,3 +18,29 @@ const bottomRight = document.querySelector("#bottomright");
 const strictButton = document.querySelector("#strict");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
+
+strictButton.addEventListener("click", function (event) {
+  if (strictButton.checked) {
+    strict = true;
+  } else {
+    strict = false;
+  }
+});
+
+onButton.addEventListener("click", function (event) {
+  if (onButton.checked) {
+    on = true;
+    turnCounter.innerHTML = "-";
+  } else {
+    on = false;
+    turnCounter.innerHTML = "";
+    clearColor();
+    clearInterval(intervalId);
+  }
+});
+
+startButton.addEventListener("click", function (event) {
+  if (on || win) {
+    play();
+  }
+});
